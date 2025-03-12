d0edcf7a7992a373e5fa9fec7806c91027ec00dd
De Raadt Tristan <tristan.deraadt@student.uclouvain.be>
2025-03-12 15:37:13 +0100

    Added main.c

---
+++ group119-tderaadt-rgoffartwinkin/build/CMakeFiles/FemPro.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FemPro.dir/src/main.c.o"
+  "CMakeFiles/FemPro.dir/src/main.c.o.d"
+  "FemPro"
+  "FemPro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FemPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
